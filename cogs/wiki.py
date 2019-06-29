fc105c203cd84b976428a53c150e481168c1792e
Alioun <14974659+Alioun@users.noreply.github.com>
2019-06-29 07:20:21 +0200

    add wiki search

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -1,14 +1,22 @@
 from discord.ext import commands
+import wikia
 
 
 class WikiCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.wiki = "kumodesu"
 
     @commands.command()
-    async def kw(self, ctx, article):
-        await ctx.send(f'Kumo wiki response for article: {article}')
+    async def kw(self, ctx, articles):
+        page = wikia.page(self.wiki, articles)
+        if page.summary.lower().startswith("redirect"):
+            redirect = " ".join(page.summary.split(" ")[1:])
+            page = wikia.page(self.wiki, redirect)
+
+        summary = page.content.split("\n")[0]
+        await ctx.send(f'**{page.title}:**\n\n{summary}\n\n*{"_".join(page.url.split(" "))}*')
 
 
 def setup(bot):
